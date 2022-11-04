79d922dd3936c84ad6c5156e7facd42eb602bdf5
Ohneptune <115102313+Ohneptune@users.noreply.github.com>
2022-11-04 13:36:58 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger');
+const app = express();
+
+
+
+// Innit Middlewarte
+//app.use(logger);
+
+
+// Gets All Members
+app.get('/api/members', (req, res) => res.json(members));
+
+//Get single member
+app.get('/api/members/:id',(req, res) => {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+});
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ public/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Gizmo',
+        email: 'gizpig@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Greta',
+        email: 'skettybabes@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Ugly',
+        email: 'miss ugly',
+        status: 'active'
+    }
+];
+
+module.exports = members;
